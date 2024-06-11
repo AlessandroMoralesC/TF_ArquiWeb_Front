a5e86c3cd085af44a3181401d980bb49f9d917be
GGstorm99 <116691271+GGstorm99@users.noreply.github.com>
2024-06-10 21:17:02 -0500

    Citas/horario

--- src/app/models/horario.ts
+++ src/app/models/horario.ts
@@ -1,1 +1,8 @@
+import { Cita } from "./cita";
 
+export class HorarioMedico {
+    idHorario: number = 0
+    fechaHorario: Date = new Date();
+    estadoHorario: string = ""
+    idCita: Cita = new Cita()
+}
