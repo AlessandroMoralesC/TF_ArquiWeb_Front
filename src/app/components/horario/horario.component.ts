cd2b80aa17cbd8587434aeb409ba42fcd247b2d1
GGstorm99 <116691271+GGstorm99@users.noreply.github.com>
2024-06-10 21:40:14 -0500

    Citas/horario

--- src/app/components/horario/horario.component.ts
+++ src/app/components/horario/horario.component.ts
@@ -1,1 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListarHorarioComponent } from './listar-horario/listar-horario.component';
+
+@Component({
+  selector: 'app-horario',
+  standalone: true,
+  imports: [RouterOutlet,ListarHorarioComponent],
+  templateUrl: './horario.component.html',
+  styleUrl: './horario.component.css'
+})
+export class HorarioComponent implements OnInit{
+  constructor(public route:ActivatedRoute) {}
+  ngOnInit(): void {}
+}
 
