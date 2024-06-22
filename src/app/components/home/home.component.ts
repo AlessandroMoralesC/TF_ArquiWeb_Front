d06807558af106000d07cf2d80cc9dbd3cc15539
J0elCLC <u202117271@upc.edu.pe>
2024-06-22 17:59:23 -0500

    r

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterLink],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: './home.component.html'  // Eliminando styleUrls si no es necesario
 })
-export class HomeComponent {
-
-}
+export class HomeComponent { }
