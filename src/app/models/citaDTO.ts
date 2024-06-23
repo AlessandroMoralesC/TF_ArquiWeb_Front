8beeb52db6714853d7aa354de006bd11844796c3
Sandro Huamani <116691271+Sandro12883@users.noreply.github.com>
2024-06-22 20:00:02 -0500

    report Cita

--- src/app/models/citaDTO.ts
+++ src/app/models/citaDTO.ts
@@ -1,1 +1,9 @@
+import { Users } from "./users"
 
+export class CitaDTO {
+    idCita: number = 0
+    fechaCita: Date = new Date()
+    motivoCita: string = ""
+    horaCita: Date = new Date()
+    usuario: Users = new Users()
+}
