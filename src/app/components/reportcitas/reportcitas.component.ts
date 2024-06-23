ea62df465e28e30bed162662677dd048b43c862e
Sandro Huamani <116691271+Sandro12883@users.noreply.github.com>
2024-06-22 20:23:55 -0500

    report Cita

--- src/app/components/reportcitas/reportcitas.component.ts
+++ src/app/components/reportcitas/reportcitas.component.ts
@@ -1,1 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ReportcitaporfechaComponent } from './reportcitaporfecha/reportcitaporfecha.component';
+
+@Component({
+  selector: 'app-reportcitas',
+  standalone: true,
+  imports: [RouterOutlet, ReportcitaporfechaComponent],
+  templateUrl: './reportcitas.component.html',
+  styleUrl: './reportcitas.component.css'
+})
+export class ReportcitasComponent implements OnInit{
+  constructor(public route: ActivatedRoute) {}
+  ngOnInit(): void {
+    
+  }
+}
 
